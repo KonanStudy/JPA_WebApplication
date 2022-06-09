28b441857227beaa7392b9efbff67f8994a72bd3
jinhee06 <lee.jinhee@konantech.com>
2022-06-09 13:19:30 +0900

    지역별 검색 수정

--- jpashop_leejinhee/src/main/java/jpabook/jpashop/search/dao/foodNationDAO.java
+++ jpashop_leejinhee/src/main/java/jpabook/jpashop/search/dao/foodNationDAO.java
@@ -19,20 +19,84 @@ public class foodNationDAO {
     Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
     public Model foodNationSearch(SearchParamVo paramVo, Model model){
+
         logger.info("foodNationSearch");
+
         try{
             String listName = "foodNationList";
             String totalName="foodNationTotal";
 
-            //SearchParamVo paramvo = (SearchParamVo)request.getAttribute("params");
             String kwd = paramVo.getKwd();
 
             //검색어 없을 경우
-            /*if(kwd.isEmpty() && paramVo.getClickCity().isEmpty()){
+            if(kwd.isEmpty() && paramVo.getClickCity().isEmpty()){
                 model.addAttribute(totalName, 0 );
                 return model;
+            }
 
-            }*/
+            //검색어 있을 경우
+            SearchRestVo restvo = new SearchRestVo();
+            RestModule module = new RestModule();
+
+            CommonUtil comUtil = new CommonUtil();
+            StringBuffer sbquery = new StringBuffer();
+            StringBuffer sbcustom = new StringBuffer();
+            String strNmFd = paramVo.getFields().isEmpty() ? "text_idx" : paramVo.getFields();
+            if("title".equals(strNmFd)) strNmFd = "restaurant_nm";
+
+
+
+            //상세검색
+            // if (paramVo.isDetail()) {
+            //    sbquery.append(comUtil.makeDetailQuery(paramVo, strNmFd));
+            //} else {  //일반검색
+                sbquery.append("region like '").append(paramVo.getClickCity()).append("*' ");
+            //}
+
+            restvo.setUrl(restvo.getSearchUrl().toString());
+            restvo.setSelectFields("region,city_nm,food_type,menu,restaurant_nm,srch_kwd,corpus,corpus_kwd");
+            restvo.setFrom("food_nation.food_nation");
+            restvo.setWhere( sbquery.toString());
+            restvo.setOffset( paramVo.getOffset() );
+            restvo.setPagelength(paramVo.getPageSize() );
+            restvo.setHilightFields("{'restaurant_nm':{'length':250,'begin':'<strong>','end':'</strong>'}},{'recommend':{'length':200,'begin':'<strong>','end':'</strong>'}}");
+            restvo.setCustomLog(comUtil.getCustomLog(paramVo)  );
+
+            logger.info(">>>>>>>>>>>>>  foodNation query: "+ restvo.toString());
+            RestResultVo resultvo = module.restSearchPost(restvo);
+            //RestResultVo resultvo = module.customRestSearchGet(restvo);
+
+
+            logger.debug(">>>>>>>>>>>>>  query-sample "+paramVo);
+            logger.debug(">>>>>>>>>>>>>  resultvo list "+resultvo.getResult() );
+            logger.debug(">>>>>>>>>>>>>  resultvo total "+resultvo.getTotal());
+
+            model.addAttribute(listName, resultvo.getResult() );
+            model.addAttribute(totalName,resultvo.getTotal());
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+            logger.error(" foodNation error - "+e.toString());
+            model.addAttribute("error", SearchConstant.MSG_SEARCH_ERROR);
+        }
+        return model;
+    }
+
+    public Model foodNationCustomSearch(SearchParamVo paramVo, Model model){
+
+       logger.info("foodNationCustomSearch");
+        try{
+            String listName = "foodNationList";
+            String totalName="foodNationTotal";
+
+            String kwd = paramVo.getKwd();
+
+            //검색어 없을 경우
+            if(kwd.isEmpty() && paramVo.getClickCity().isEmpty()){
+                model.addAttribute(totalName, 0 );
+                return model;
+            }
 
                 //검색어 있을 경우
                 SearchRestVo restvo = new SearchRestVo();
@@ -45,66 +109,11 @@ public class foodNationDAO {
                 if("title".equals(strNmFd)) strNmFd = "restaurant_nm";
 
                 //상세검색
-                if (paramVo.isDetail()) {
-                    sbquery.append(comUtil.makeDetailQuery(paramVo, strNmFd));
-                } else {  //일반검색
-                    //sbquery.append(strNmFd);
-                    ////sbquery.append(" = '").append(kwd).append("' allword synonym ");
-                    //if("text_idx".equals(strNmFd)){
-                    //    sbquery.append(" = '").append(kwd).append("' anyword");
-                    //}else{
-                    //    sbquery.append(" like '*").append(kwd).append("*' ");
-                    //}
+                //if (paramVo.isDetail()) {
+                //    sbquery.append(comUtil.makeDetailQuery(paramVo, strNmFd));
+                //} else {  //일반검색
                     sbquery.append(kwd);
-                }
-
-                if (!kwd.isEmpty() && !paramVo.getClickCity().isEmpty()) {
-                    //sbquery.append(" and region = '").append(paramVo.getClickCity()).append("' ");
-                    sbquery.insert(0,paramVo.getClickCity().toString() + " ");
-                }else if(kwd.isEmpty() && !paramVo.getClickCity().isEmpty()){
-                    sbquery.setLength(0);
-                    //sbquery.append("region= '").append(paramVo.getClickCity()).append("' ");
-                    sbquery.append(paramVo.getClickCity());
-                }else if(kwd.isEmpty() && paramVo.getClickCity().isEmpty()){
-                    model.addAttribute(totalName, 0 );
-                    return model;
-                }
-
-                //결과내재검색
-                if (paramVo.isResrch()) {
-                    if (sbquery.length() > 0) sbquery.append(" and ");
-                    sbquery.append(comUtil.makeReSearchQuery(paramVo, strNmFd));
-                }
-
-                //범위검색
-                if (!paramVo.getStartDate().isEmpty()) {
-                    sbquery.append(" and created_ymd >= '").append(paramVo.getStartDate()).append("' ");
-                }
-                if (!paramVo.getEndDate().isEmpty()) {
-                    sbquery.append(" and created_ymd <= '").append(paramVo.getEndDate()).append("' ");
-                }
-
-
-                //정렬조건 d:최신순, r: 정확도순, c: 클릭순
-            /*switch(paramVo.getSort()){
-                case "d":
-                    sbquery.append(" order by created_ymd desc ");
-                    break;
-                case "r":
-                    sbquery.append(" order by $relevance desc ");
-                    break;
-                case "c":
-                    String docids = module.getKeywordDocidRank(kwd, "sample", "w");
-                    if(!docids.isEmpty()){
-                        sbquery.append(" order by post_id (").append(docids).append(") ");
-                    }
-                    break;
-                default:
-                    sbquery.append(" order by created_ymd desc ");
-                    break;
-            }*/
-
-
+                //}
 
             restvo.setUrl(restvo.getCustomSearchUrl().toString());
             restvo.setSelectFields("region,city_nm,food_type,menu,restaurant_nm,srch_kwd,corpus,corpus_kwd");
@@ -112,8 +121,8 @@ public class foodNationDAO {
             restvo.setWhere( sbquery.toString());
             restvo.setOffset( paramVo.getOffset() );
             restvo.setPagelength(paramVo.getPageSize() );
-            restvo.setHilightFields("{'restaurant_nm':{'length':250,'begin':'<strong>','end':'</strong>'}},{'recommend':{'length':200,'begin':'<strong>','end':'</strong>'}}");
-            restvo.setCustomLog(comUtil.getCustomLog(paramVo)  );
+            //restvo.setHilightFields("{'restaurant_nm':{'length':250,'begin':'<strong>','end':'</strong>'}},{'recommend':{'length':200,'begin':'<strong>','end':'</strong>'}}");
+            //restvo.setCustomLog(comUtil.getCustomLog(paramVo)  );
 
             logger.info(">>>>>>>>>>>>>  foodNation query: "+ restvo.toString());
             //RestResultVo resultvo = module.restSearchPost(restvo);
@@ -146,27 +155,12 @@ public class foodNationDAO {
 
             CommonUtil comUtil = new CommonUtil();
             StringBuffer sbquery = new StringBuffer();
-            StringBuffer sbcustom = new StringBuffer();
             //String strNmFd = paramVo.getFields().isEmpty() ? "text_idx" : paramVo.getFields();
 
             String kwd = map.get("kwd").toString();
-            String region = map.get("region").toString();
+            //String region = map.get("region").toString();
 
-
-            if(!kwd.isEmpty()){
-                sbquery.append(kwd);
-            }
-
-            if (!kwd.isEmpty() && !region.isEmpty()) {
-                //sbquery.append(" and region = '").append(paramVo.getClickCity()).append("' ");
-                sbquery.insert(0,region + " ");
-            }else if(kwd.isEmpty() && !region.isEmpty()){
-                sbquery.setLength(0);
-                //sbquery.append("region= '").append(paramVo.getClickCity()).append("' ");
-                sbquery.append(region);
-            }
-
-            //sbquery.append(" group by region order by count(*) desc");
+            sbquery.append(kwd);
 
             restvo.setUrl(restvo.getCustomMapUrl().toString());
             restvo.setSelectFields("region,count(*)");
